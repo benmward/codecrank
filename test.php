a1900cedd2afaefbf110fa20f456a6cab7242f3b
Nathan LaPierre <nathanl2012@gmail.com>
2014-10-26 09:00:18 -0400

    new jquery ajax php test 3

--- test.php
+++ test.php
@@ -1,16 +1,8 @@
-<html>
-    <head>
-        <title>Just a php test</title>
-    </head>
-    <body>
-        <a href="#" id="showContent" >show content</a>
-        <?php
-            echo "this is a test!!!"
-            //$source = $_POST['pageSource'];
-            //print $source;
-            //print "hello print!";
-            //echo "hello echo!";
-            //return "hello!";
-        ?>
-    </body>
-</html>
+<?php
+    //echo "this is a test!!!";
+    $source = $_POST['pageSource'];
+    print $source;
+    //print "hello print!";
+    //echo "hello echo!";
+    //return "hello!";
+?>
