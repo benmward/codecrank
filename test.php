e7023abf75f8adbefa086431cd246f7be3ff761a
Nathan LaPierre <nathanl2012@gmail.com>
2014-10-26 09:14:04 -0400

    new jquery ajax php test 4

--- test.php
+++ test.php
@@ -1,8 +1,16 @@
 <?php
-    //echo "this is a test!!!";
-    $source = $_POST['pageSource'];
-    print $source;
-    //print "hello print!";
-    //echo "hello echo!";
-    //return "hello!";
+//echo "this is a test!!!";
+//if($_POST['pageSource'] == True)
+//{
+//    echo "success!";
+//}
+//else
+//{
+//    echo "failure...";
+//}
+$source = $_POST['pageSource'];
+print $source;
+//print "hello print!";
+//echo "hello echo!";
+//return "hello!";
 ?>
